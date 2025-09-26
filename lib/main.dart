d36aae2f5518256e726861fe42662415f41d703d
SomratSH <ahmedsomrat945@gmail.com>
2025-09-26 15:28:28 +0600

    all done fronted

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:voltly_app/presentation/common_page/onboarding/splash_screen.dart';
 import 'package:voltly_app/presentation/station_owner/landing_page/landing_owner_page.dart';
 import 'package:voltly_app/presentation/user/landing_page/landing_page.dart';
 import 'package:voltly_app/presentation/user/station/add_charing.dart';
 import 'package:voltly_app/presentation/user/station/connect_charger.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
@@ -15,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Color(0xFF121C24),
         fontFamily: 'Roboto',
